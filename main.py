a30a498ad45a8588c777e5e991916529269cba47
harrki <k19016kk@aitech.ac.jp>
2020-11-26 17:23:09 +0900

    自分の日記を追加しました

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from diaries.DiarySample import DiarySample
 from diaries.HisamatsuDiary import HisamatsuDiary
 from diaries.SobueDiarynew import SobueDiarynew
 from diaries.msd05keisuke_diary import msd05keisuke_diary
+from diaries.HarukiDiary import HarukiDiary
 
-diaries = [DiarySample(),HisamatsuDiary(), msd05keisuke_diary(), SobueDiarynew(), ]
+diaries = [DiarySample(), HisamatsuDiary(), msd05keisuke_diary(), SobueDiarynew(), HarukiDiary(), ]
 
 for d in diaries:
     print("---------------------------------")
