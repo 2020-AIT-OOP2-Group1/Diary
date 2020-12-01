289813cd137a80c3dd125bd7858fc76d08f296ac
tanaka <75057595+hyt-gij8810@users.noreply.github.com>
2020-12-01 12:18:29 +0900

    確認お願いします。

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from diaries.msd05keisuke_diary import msd05keisuke_diary
 from diaries.SobueDiarynew import SobueDiarynew
 from diaries.shiratoDiary import shiratoDiary
 from diaries.HarukiDiary import HarukiDiary
+from diaries.Tanaka_new import Tanaka_new
 
-diaries = [DiarySample(), HisamatsuDiary(), msd05keisuke_diary(), SobueDiarynew(), shiratoDiary(), HarukiDiary(), ]
+diaries = [DiarySample(), HisamatsuDiary(), msd05keisuke_diary(), SobueDiarynew(), shiratoDiary(), HarukiDiary(), Tanaka_new()]
 
 for d in diaries:
     print("---------------------------------")
